f35d6b7714db36a24b542b2763da8775a1651d64
halilibrahimdemircan <58706397+halilibrahimdemircan@users.noreply.github.com>
2023-09-18 14:42:56 +0300

    plate created

--- SuperHeroApiDotNet7/SuperHeroApiDotNet7/Program.cs
+++ SuperHeroApiDotNet7/SuperHeroApiDotNet7/Program.cs
@@ -1,4 +1,5 @@
 ﻿global using SuperHeroApiDotNet7.Models;
+global using SuperHeroApiDotNet7.Data;
 using SuperHeroApiDotNet7.Services.SuperHeroService;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
+builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
 
